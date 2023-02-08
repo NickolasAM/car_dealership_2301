6b64c3d24c5d99390db251fa9797611b1a8be1e3
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-08 10:54:15 -0700

    Splitting method

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,37 +2,37 @@ require 'rspec'
 require './lib/car'
 
 RSpec.describe Car do
-  it 'exists' do
-    car = Car.new("Ford Mustang", 1500, 36)
+  describe '#exists'  
+    it 'exists' do
+      car = Car.new("Ford Mustang", 1500, 36)
 
-    expect(car).to be_a Car
+      expect(car).to be_an_instance_of(Car)
+    end
   end
 
-  it 'has a make' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.make).to eq("Ford")
+  describe '#has attributes'
+    it 'split_make_model' do
+      car = Car.new("Ford Mustang", 1500, 36)
+      
+      expect(car.make_model).to eq("Ford Mustang")
+      expect(car.make).to eq("Ford")
+      expect(car.model).to eq("Mustang")
+    end
   end
 
-  it 'has a model' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.model).to eq("Mustang")
-  end
-
-  it 'has a monthly payment' do
+  xit 'has a monthly payment' do
     car = Car.new("Ford Mustang", 1500, 36)
 
     expect(car.monthly_payment).to eq(1500)
   end
 
-  it 'has a loan length' do
+  xit 'has a loan length' do
     car = Car.new("Ford Mustang", 1500, 36)
 
     expect(car.loan_length).to eq(36)
   end
 
-  it 'has a total cost' do
+  xit 'has a total cost' do
     car = Car.new("Ford Mustang", 1500, 36)
 
     expect(car.total_cost).to eq(54000)
